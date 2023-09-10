54dc318362214b1451c32608fc1149283a24e2fa
Oleg Novoselskyy <tatteddev@gmail.com>
2023-09-10 16:48:53 -0400

    more flexbox

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -5,10 +5,10 @@ export default function App() {
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder='Course Goal' style={styles.textInput} />
-        <Button title='Add Goal' />
+        <TextInput placeholder="Course Goal" style={styles.textInput} />
+        <Button title="Add Goal" />
       </View>
-      <View>
+      <View style={styles.goalList}>
         <Text>Goal List</Text>
       </View>
     </View>
@@ -17,17 +17,27 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16
   },
   inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
   },
   textInput: {
     borderWidth: 1,
-    borderColor: '#cccccc',
-    width: '80%',
+    borderColor: "#cccccc",
+    width: "70%",
     marginRight: 10,
     padding: 10,
-  }
+  },
+  goalList: {
+    flex: 5
+  },
 });
