9b2fe7656fe60da482b212806bf25b1c3d090044
Oleg Novoselskyy <tatteddev@gmail.com>
2023-09-10 15:15:07 -0400

    button

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello, World!</Text>
+      <Text>Hello, World!!!!</Text>
+      <Button title='Click Me!'/>
       <StatusBar style="auto" />
     </View>
   );
